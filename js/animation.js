ff48eec4444de13c400db4701369a23cd6427113
Wojciech Pasternak <wojciech.pasternakk@gmail.com>
2022-12-12 11:25:20 +0100

    final commit

---
+++ js/animation.js
@@ -0,0 +1,21 @@
+// Animation 
+    // Logo 
+    ScrollReveal().reveal('.primary-info__logo-image', {delay: 500, duration: 2000, scale: 1, rotate:{x:150}});
+    
+    // Second info - Left Section 
+    ScrollReveal().reveal('.second-info__left', {delay: 200, duration: 1500, distance: '50px', origin: 'left'});
+
+    // Second info - Right Section 
+    ScrollReveal().reveal('.second-info__right', {delay: 1000, duration: 1500, distance: '100px', origin: 'right'});
+
+    // Presentation - Right Title 
+    ScrollReveal().reveal('.presentation__title', {delay: 1000, duration: 1500, distance: '100px', origin: 'left'});
+
+    // Presentation - Left Title 
+    ScrollReveal().reveal('.presentation__title--left', { delay: 700, duration: 1500, distance: '300px', origin: 'right'});
+
+    // Palace
+    ScrollReveal().reveal('.palace', {delay: 500, duration: 1500, rotate:{y:180}});
+
+    // Gallery
+    ScrollReveal().reveal('.gallery', {delay: 500, duration: 1000, scale:1});
